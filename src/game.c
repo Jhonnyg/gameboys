96c1cdf522d0b3eeb3a8c43f78fdb35207e06f6c
Jhonny Göransson <jhonny.goransson@gmail.com>
2013-11-04 22:43:17 +0100

    Added: A bunch of GB examples i found.

--- src/game.c
+++ src/game.c
@@ -1,7 +1,24 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
+/* game includes */
+#include "../include/sprites.h"
+
 void main()
 {
-    printf("GAMEBOYS IN THE CLUB!!");
+	/* martin timell tells the time */
+	int frame = 0;
+
+	/* pre-draw setup code */
+    DISPLAY_ON;
+  	enable_interrupts(); 
+
+  	while(1) 
+  	{
+  		/* wait for vertical blank, guess it's a complete screen redraw sync */
+  		wait_vbl_done();
+
+  		
+  		frame++;
+  	}
 }
