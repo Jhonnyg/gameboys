6f9ec2f23c85d73ae026988d240fff6e3afbba60
Johannes Algelind <johannes@algelind.se>
2013-11-07 21:48:19 +0100

    Oops. Forgot to add the source files.

---
+++ include/text.h
@@ -0,0 +1,6 @@
+#ifndef __TEXT_H__
+#define __TEXT_H__
+
+void draw_text(const char* text);
+
+#endif
---
+++ src/text.c
@@ -0,0 +1,19 @@
+#include <gb/gb.h>
+#include <gb/drawing.h>
+
+void draw_text(const char* text)
+{
+    const int x = 1;
+    const int y = 16;
+    const offset = 5;
+    const x1 = offset, y1 = GRAPHICS_HEIGHT - offset - 16;
+    const x2 = GRAPHICS_WIDTH - offset, y2 = GRAPHICS_HEIGHT - offset;
+    color(BLACK, WHITE, SOLID);
+    box(x1, y1, x2, y2, M_NOFILL);
+    color(LTGREY, WHITE, SOLID);
+    line(x1+1, y1+1, x2-1, y1 + 1);
+    line(x1+1, y2-1, x2-1, y2 - 1);
+    gotogxy(x, y);
+    color(BLACK, WHITE, SOLID); 
+    gprintf(text);
+}
