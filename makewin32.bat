8d4df92145eae787e9d44098b83564e45ad8006e
Johannes Algelind <johannes@algelind.se>
2013-11-09 17:28:15 +0100

    Makefile update, win32

--- makewin32.bat
+++ makewin32.bat
@@ -2,14 +2,16 @@ setlocal
 set PATH=%PATH%;gbdk/bin/
 
 :: prepare generated sources
-python build/pack_sprites.py
+
+mkdir build\gen
+call "build/pack_sprites.py"
 
 :: compile game files
-lcc -Iinclude -c -o build/input.o src/input.c
-lcc -Iinclude -c -o build/sound.o src/sound.c
-lcc -Iinclude -c -o build/game.o src/game.c
-lcc -Iinclude -c -o build/sprite_manager.o src/sprite_manager.c
-lcc -Iinclude -c -o build/text.o src/text.c
+lcc -Iinclude -Ibuild/gen -c -o build/input.o src/input.c
+lcc -Iinclude -Ibuild/gen -c -o build/sound.o src/sound.c
+lcc -Iinclude -Ibuild/gen -c -o build/game.o src/game.c
+lcc -Iinclude -Ibuild/gen -c -o build/sprite_manager.o src/sprite_manager.c
+lcc -Iinclude -Ibuild/gen -c -o build/text.o src/text.c
 
 :: link into rom
 lcc -o build/game.gb build/game.o build/input.o build/sound.o build/sprite_manager.o build/text.o
